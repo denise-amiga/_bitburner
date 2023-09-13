18b24dd86be8ddec7a88b90581032a167713d2cd
Denise <denise-amiga@users.noreply.github.com>
2023-09-13 22:54:49 +0200

    Update e.js
    Padding numbers

--- e.js
+++ e.js
@@ -1,17 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
   ns.tail();
-  ns.resizeTail(300, 200);
+  ns.resizeTail(300, 250);
   ns.moveTail(740, 0);
   let level = ns.args.length > 0 ? ns.args[0] : ns.getHackingLevel();
   let force = ns.args.length > 1 ? false : true;
   ns.disableLog("ALL");
-  let servers = serverList(ns);
+  let servers = serverList(ns).filter((f) => !f.includes("zz-") && !f.includes("home"));
   for (let s of servers) {
-    if (s.includes("zz") || s.includes("home")) continue
+    //    if (s.includes("zz") || s.includes("home")) continue
     if (ns.hasRootAccess(s) && force) continue
     if (level < ns.getServerRequiredHackingLevel(s)) continue
-    ns.print(ns.getServerRequiredHackingLevel(s), ":", ns.getServerNumPortsRequired(s), " >> ", s);
+    ns.print(ns.getServerRequiredHackingLevel(s).toString().padStart(4, " "), ":", ns.getServerNumPortsRequired(s), " >> ", s);
   }
 }
 
